7c1a2bed9b62fd1d8c681690bc089c21e766169f
jungbin97 <ljb5054@gmail.com>
2023-12-10 15:56:40 +0900

    chore: 스프링 기본세팅 및 의존성 추가

---
+++ src/main/java/fluffysnow/idearly/IdearlyApplication.java
@@ -0,0 +1,13 @@
+package fluffysnow.idearly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdearlyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdearlyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fluffysnow/idearly/IdearlyApplicationTests.java
@@ -0,0 +1,13 @@
+package fluffysnow.idearly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdearlyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

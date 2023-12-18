5f06a97da40a0f8c9d17d0e11c140147eb35cd4f
jungbin97 <ljb5054@gmail.com>
2023-12-18 16:24:13 +0900

    feat: TestCaseInfo Dto 생성 - TestCase id와 채점 상태의 정보를 담습니다.

---
+++ src/main/java/fluffysnow/idearly/problem/dto/submit/TestCaseInfo.java
@@ -0,0 +1,19 @@
+package fluffysnow.idearly.problem.dto.submit;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Getter
+public class TestCaseInfo {
+    private Long testCaseId;
+    private String status;  // pass, failed, error, timeout
+
+    // 정적 팩토리 메서드로 해당 Dto를 생성합니다.
+    public static TestCaseInfo of(Long testCaseId, String status) {
+        return new TestCaseInfo(testCaseId, status);
+    }
+}

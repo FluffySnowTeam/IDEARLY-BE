7b3ede12b4b8d4e3e7c8c04e0dec575d2e4fa204
youngmin <dudals9901@naver.com>
2023-12-24 09:48:36 +0900

    fix. CompetitionList 조회시 참가 가능한 대회와 불가능한 대회를 따로 조회하도록 변경

--- src/main/java/fluffysnow/idearly/competition/repository/CompetitionRepository.java
+++ src/main/java/fluffysnow/idearly/competition/repository/CompetitionRepository.java
@@ -2,8 +2,17 @@ package fluffysnow.idearly.competition.repository;
 
 import fluffysnow.idearly.competition.domain.Competition;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
-import java.util.Optional;
+import java.time.LocalDateTime;
+import java.util.List;
 
 public interface CompetitionRepository extends JpaRepository<Competition, Long> {
+
+    @Query("select c from Competition c where c.endDatetime >= :now order by c.startDatetime")
+    List<Competition> findAvailableCompetitionList(@Param("now") LocalDateTime now);
+
+    @Query("select c from Competition c where c.endDatetime < :now order by c.startDatetime")
+    List<Competition> findUnavailableCompetitionList(@Param("now") LocalDateTime now);
 }
